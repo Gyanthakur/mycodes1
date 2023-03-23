bea623bd68739ecbd05b16e42a62d6d3849d02f9
gyanthakur <gps.9616@gmail.com>
2023-03-23 14:49:11 +0530

    first commit

---
+++ stl/practiceSTL.cpp
@@ -0,0 +1,112 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+/*
+    vector<int>v;
+    v.push_back(10);
+    v.push_back(1123);
+    v.push_back(15);
+    v.emplace_back(42);
+    v.emplace_back(23);
+    v.emplace_back(278);
+    vector<int> v2(v);
+
+    vector<int>::iterator it=v.begin();
+    it++;
+    cout<< *it <<endl;
+    vector<int>::iterator it1= v.end();
+    // vector<int>::iterator it= v.end();
+    cout<<*(it1)<<" ";
+    it1+=2;
+    cout<<endl;
+
+    v2.erase(v2.begin()+1);
+    v2.erase(v2.begin()+1,v2.begin()+4);
+    v2.push_back(34);
+    v2.push_back(341);
+    v2.push_back(342);
+    v2.push_back(343);
+    v2.pop_back();
+    v2.pop_back();
+    v2.pop_back();
+    for(auto it=v2.begin();it!=v2.end();it++)
+    {
+        cout<<*(it)<<" ";
+    } 
+
+    // it++;
+    cout<<endl;
+
+    // vector<pair<int,int>>vp;
+    // vp.push_back({2,4});
+    // vp.push_back({2,4});
+    // vp.push_back({2,4});
+    
+
+*/
+
+
+
+/*
+
+
+
+// lern list 
+    list<int>ls;
+    ls.push_back(2);
+    ls.push_front(1);
+    ls.push_front(14);
+    ls.emplace_back(4);
+    ls.emplace_front(01);
+    ls.pop_back();
+    ls.pop_front();
+    
+
+    for(auto it = ls.begin();it!=ls.end();it++)
+    {
+        cout<<*(it)<<" ";
+    }
+    cout<<endl;
+    cout<<ls.size()<<endl;
+
+    // rest all list function like that vector
+    // begin,end,rend,rbegin,swap,insert,size
+
+
+
+
+*/
+
+
+
+/*
+
+
+// learn deque 
+
+    deque<int>d;
+    d.push_back(19);
+    d.push_front(1);
+    d.emplace_back(18);
+    d.emplace_front(2);
+    swap(d[2],d[1]);
+    for(auto it=d.begin();it!=d.end();it++)
+    {
+        cout<<*(it) <<" ";
+    }
+    cout<<endl;
+    cout<<d.size()<<endl;
+
+    // rest function same as vector 
+    // push,pop,insert,swap,size,end,begin
+
+
+
+    */
+
+
+
+   
+    return 0;
+}
